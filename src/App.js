4f8ddaa4db00c4b537eb5bf742a6522aa588e927
AurelienVERRIER <aurelien.verrier@gmail.com>
2022-07-18 12:22:33 +0200

    state set up in constructor

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import React from 'react';
 import './App.css';
 
-function App() {
+class App extends React.component {
+
+constructor () {
+  super ()
+  this.state = {
+    email: "",
+    password: "",
+    rememberMe: false,
+    emailIsValid: false,
+    passwordIsValid: false,
+    isSubmitted: false
+  }
+}
+
+render () {
   return (
     <>
       <h1>test</h1>
     </>
   );
+}
 }
 
 export default App;
