f8a4537db3d5dcbe29af528bae39d1bbf0ac1562
AurelienVERRIER <aurelien.verrier@gmail.com>
2022-07-18 16:51:59 +0200

    exercise 2 almost done

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App extends React.Component {
 constructor () {
   super ()
   this.state = {
+    firsName: "",
     email: "",
     password: "",
     rememberMe: false,
@@ -16,61 +17,99 @@ constructor () {
 }
 
 
-// emailsValid = /^[\w\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(this.state.email);
-
 
 
 
 handleEmailChange = (e) => {
   this.setState({email: e.target.value})
-  if ('/^[\w\.]+@([\w-]+\.)+[\w-]{2,4}$/'.test(this.state.email)) {
-    this.setState({emailIsValid: "true"})
+  if (/^[\w\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(this.state.email)) {
+    this.setState({emailIsValid: true})
 }
 }
 
 handlePasswordChange = (e) => {
-  this.state({password: e.target.value})
-  
+  this.setState({password: e.target.value})
+
   if (this.state.password.length >5) {
-    this.setState({passwordIsValid:"true"})
+    this.setState({passwordIsValid: true})
   }
 }
 
 handleRememberMeChange = (e) => {
-  this.setState({rememberMe: "true"})
+  this.setState({rememberMe: true})
 }
 
 handleSubmit = (e) => {
   e.preventDefault();
   if (this.state.emailIsValid && this.state.passwordIsValid) {
-    this.setState({isSubmitted: "true"})
+    this.setState({isSubmitted: true})
   } 
 }
 
+
+
 render () {
+
+  const emailIsValid = this.state.emailIsValid
+  const passwordIsValid = this.state.passwordIsValid
+
   return (
     <>
+      
+      <form onSubmit={this.handleSubmit}>
 
-      <form>
+        <div className="form-group">
+          <label htmlFor="inputFirstName">First Name</label>
+          <input type="text" onChange={this.handleLastNameChange} className="form-control" id="inputFirstName"  placeholder="Enter your First Name"/>
+        </div>
 
-        <div class="form-group">
-          <label for="inputEmail1">Email address</label>
-          <input type="email" onChange={this.handleEmailChange} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+        {emailIsValid ? (
+
+        <div className="form-group">
+          <label htmlFor="inputEmail1">Email address</label>
+          <input type="email" onChange={this.handleEmailChange} className="form-control is-valid" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
         </div>
-        
-        <div class="form-group">
-          <label for="inputPassword1">Password</label>
-          <input type="password" onChange={this.handlePasswordChange} className="form-control" id="exampleInputPassword1" placeholder="Password"/>
+
+        ) : (
+
+        <div className="form-group">
+          <label htmlFor="inputEmail1">Email address</label>
+          <input type="email" onChange={this.handleEmailChange} className="form-control is-invalid" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
         </div>
+
+        )}
         
-        <div class="form-group form-check">
+        
+        {passwordIsValid ? (
+
+        <div className="form-group">
+          <label htmlFor="inputPassword1">Password</label>
+          <input type="password" onChange={this.handlePasswordChange} className="form-control is-valid" id="inputPassword1" placeholder="Password"/>
+        </div>
+
+        ) : (
+
+        <div className="form-group">
+          <label htmlFor="inputPassword1">Password</label>
+          <input type="password" onChange={this.handlePasswordChange} className="form-control is-invalid" id="inputPassword1" placeholder="Password"/>
+        </div>
+
+        )}
+        
+        
+        
+
+        
+        <div className="form-group form-check">
           <input type="checkbox" onChange={this.handleRememberMeChange} className="form-check-input" id="exampleCheck1"/>
-          <label class="form-check-label" for="exampleCheck1">Remember me</label>
+          <label className="form-check-label" htmlFor="exampleCheck1">Remember me</label>
         </div>
         
-        <button type="submit" onSubmit={this.handleSubmit} className="btn btn-primary">Submit</button>
+        <button type="submit"  className="btn btn-primary">Submit</button>
       
       </form>
+
+      {/* )} */}
 
     </>
   )
